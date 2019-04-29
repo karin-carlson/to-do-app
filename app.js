e2047958c52a3b6a11b7a2746ee99ed8f2d91b9a
karin-carlson <38005879+karin-carlson@users.noreply.github.com>
2019-04-29 15:07:01 -0400

    added delete button functionality

--- app.js
+++ app.js
@@ -14,19 +14,25 @@
 
     // create a new input
     let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
     checkbox.type = "checkbox";
+      
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+      
+   deleteBtn.addEventListener('click', function(event){
+    toDoList.removeChild(this.parentElement);
+    })
 
-          // set the title
+    // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
       
-          // attach the li to the ul
+    // attach the li to the ul
     toDoList.appendChild(newLi);
       
+    newLi.appendChild(deleteBtn);
 
     //empty the input
     newToDoText.value = '';
